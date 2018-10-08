cba04e1b5b41d12b50fc71960314f11213b866ec
Shepico <Shepico@gmail.com>
2018-10-08 16:14:23 +0400

    Созданы все классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main (String[] args) {
 
+    public static void main (String[] args) {
+        TestStart.start(ClassTestOne.class); //Объект типа класс
+        TestStart.start("ClassTestTwo"); //Имя класса
     }
 }
---
+++ src/TestStart.java
@@ -0,0 +1,18 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+
+public class TestStart {
+
+    public static void start(Object obj) {
+
+    }
+
+    @Test
+    private void isNull() {
+
+    }
+}
